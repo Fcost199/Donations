117110d45c595392eb954b28e68d3c9a62ee229d
Fcost199 <costasfernando199@gmail.com>
2020-10-05 11:05:18 -0300

    create and DER

--- src/model/bd/createTables.sql
+++ src/model/bd/createTables.sql
@@ -1,8 +1,9 @@
+use donations;
 create table doacao (
 id int AUTO_INCREMENT primary key,
 valor int NOT NULL,
 data_cadastro TIMESTAMP NOT NULL DEFAULT NOW()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
 
 create table doador (
 id int AUTO_INCREMENT primary key,
@@ -14,20 +15,23 @@ data_nascimento date,
 data_criacao TIMESTAMP NOT NULL DEFAULT NOW(),
 status boolean,
 endereco varchar(255)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
 
 create table forma_pagamento(
 id int AUTO_INCREMENT primary key,
 nome varchar(255),
 status boolean,
 data_criacao TIMESTAMP NOT NULL DEFAULT NOW()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
 
 create table doador_doacao(
 id int AUTO_INCREMENT primary key,
-id_doador int references doador(id),
-id_doacao int references doacao(id),
-id_forma_pagamento int references forma_pagamento(id),
+id_doador int(11),
+id_doacao int(11),
+id_forma_pagamento int(11),
 status boolean,
-data_criacao TIMESTAMP NOT NULL DEFAULT NOW()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+data_criacao TIMESTAMP NOT NULL DEFAULT NOW(),
+foreign key (id_doador) references doador(id),
+foreign key (id_doacao) references doacao(id),
+foreign key (id_forma_pagamento) references forma_pagamento(id)
+) ENGINE=InnoDB;
